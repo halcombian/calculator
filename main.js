1155960fbf209f7fc2695094d4f1079734f357f2
halcombian <103285162+halcombian@users.noreply.github.com>
2022-05-13 00:10:41 -0400

    Add operators array and for loop

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-const numberId = [
-	"zero",
-	"one",
-	"two",
-	"three",
-	"four",
-	"five",
-	"six",
-	"seven",
-	"eight",
-	"nine",
-];
-
 const gridContainer = document.getElementById("grid-container");
-const numberDiv = document.getElementsByClassName("number-btns");
 
-for (let i = 0; i < numberId.length; i++) {
+//Creates each number button element and assigns class and ids
+const numberBtn = document.getElementsByClassName("number-btns");
+for (let i = 0; i <= 9; i++) {
 	const createDiv = document.createElement("div");
 	gridContainer.append(createDiv);
 	createDiv.className = "number-btns";
-	numberDiv[i].textContent = i;
+	createDiv.id = "btn-" + i;
+	numberBtn[i].textContent = i;
+}
+
+//Gives operator strings for operator-btns ids and textContent
+const operators = ["+", "-", "*", "/"];
+
+//Creates each operator button element and assigns class and ids
+const operatorBtn = document.getElementsByClassName("operator-btns");
+for (let j = 0; j < operators.length; j++) {
+	const createDiv = document.createElement("div");
+	gridContainer.append(createDiv);
+	createDiv.className = "operator-btns";
+	createDiv.id = "btn-" + operators[j];
+	operatorBtn[j].textContent = operators[j];
 }
